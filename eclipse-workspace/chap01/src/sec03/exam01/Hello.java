c6d3b4dd77c838cce839e681254084b4cec1ce35
인선 강 <sunink1@naver.com>
2021-06-25 21:06:39 +0900

    혼자공부하는 자바 chap.01

---
+++ eclipse-workspace/chap01/src/sec03/exam01/Hello.java
@@ -0,0 +1,10 @@
+package sec03.exam01;
+
+public class Hello {	//파일명과 동일해야 함.
+
+	public static void main(String[] args) {	//String의 S는 대문자
+		System.out.println("Hello, Java"); 		//System의 S는 대문자
+
+	}
+
+}
